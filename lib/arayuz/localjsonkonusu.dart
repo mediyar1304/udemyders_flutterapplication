caa1909c2ec8f34bd35a54c355bce437b5814903
Mediyar Bedirhan Deniz <mediyardeniz.1304@gmail.com>
2022-08-20 13:24:09 +0300

    Add Local Json

--- lib/arayuz/localjsonkonusu.dart
+++ lib/arayuz/localjsonkonusu.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: unused_import, use_key_in_widget_constructors, prefer_const_constructors, prefer_interpolation_to_compose_strings
 
 import 'package:flutter/material.dart';
 import 'dart:convert';
@@ -9,8 +9,45 @@ class LocalJsonKonusu extends StatefulWidget {
 }
 
 class LocalState extends State<LocalJsonKonusu> {
+  late List ogrenciler;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Local Json Konusu",
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(
+          20.0,
+        ),
+        child: Center(
+          child: FutureBuilder(
+            future: DefaultAssetBundle.of(context).loadString(
+              "assets/veriler/sinif.json",
+            ),
+            builder: (context, cevap) {
+              var ogrenci = jsonDecode(cevap.data.toString());
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          "Adı: " + ogrenci[index]['adi'],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: ogrenci == null ? 0 : ogrenci.length,
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
