bfbddc9a194bdbd5c7c86875488c221b5fdaed0e
Mediyar Bedirhan Deniz <mediyardeniz.1304@gmail.com>
2022-08-23 18:14:54 +0300

    El Feneri Oluşturma

--- lib/arayuz/elfeneri.dart
+++ lib/arayuz/elfeneri.dart
@@ -1,3 +1,18 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+
+class ElFeneri extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FenerState();
+  }
+}
+
+class FenerState extends State<ElFeneri> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
