86cb6b1f98a9f0e804b61319e74e3cba126d73ad
Mediyar Bedirhan Deniz <mediyardeniz.1304@gmail.com>
2022-08-23 18:19:40 +0300

    El Feneri Yapma

--- lib/arayuz/elfeneri.dart
+++ lib/arayuz/elfeneri.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, use_key_in_widget_constructors
+// ignore_for_file: unused_import, use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -12,7 +12,8 @@ class ElFeneri extends StatefulWidget {
 class FenerState extends State<ElFeneri> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: Text("data")),
+    );
   }
 }
